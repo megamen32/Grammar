4931c3cd864b14538b77ae2cb27a7113c483dc81
Careviolan <megamen932@gmail.com>
2018-04-15 00:16:48 +0300

    начало плагиата

--- main.cpp
+++ main.cpp
@@ -3,38 +3,42 @@
 #include <map>
 #include <sstream>
 #include <string>
+typedef int Number;
 
 enum Token_value : char {
-  NAME,             NUMBER,                 END,
-  PLUS='+',         MINUS='-',              MUL='*',        DIV='/',
-  PRINT=';',        ASSIGN='=',             LP='(',         RP=')'
+  NAME, NUMBER, END,
+  PLUS = '+', MINUS = '-', MUL = '*', DIV = '/',
+  PRINT = ';', ASSIGN = '=', LP = '(', RP = ')', FUNC = 'f'
 };
 
 enum Number_value : char {
-  NUM0='0', NUM1='1', NUM2='2',
-  NUM3='3', NUM4='4', NUM5='5',
-  NUM6='6', NUM7='7', NUM8='8',
-  NUM9='9', NUMP='.',
+  NUM0 = '0', NUM1 = '1', NUM2 = '2',
+  NUM3 = '3', NUM4 = '4', NUM5 = '5',
+  NUM6 = '6', NUM7 = '7', NUM8 = '8',
+  NUM9 = '9',
 };
 
 Token_value curr_tok = PRINT;        // Хранит последний возврат функции get_token().
-double number_value;                 // Хранит целый литерал или литерал с плавающей запятой.
+Number number_value;                 // Хранит целый литерал или литерал с плавающей запятой.
 std::string string_value;            // Хранит имя.
-std::map<std::string, double> table; // Таблица имён.
+std::map<std::string, Number> table; // Таблица имён.
 int no_of_errors;                    // Хранит количество встречаемых ошибок.
+//TODO class ERROR&&ERROR_PRINTER
 
-double expr(std::istream*, bool);    // Обязательное объявление.
+Number expr(std::istream *, bool);    // Обязательное объявление.
 
 /****************************************************************************/
 
+void print(Token_value value);
 // Функция error() имеет тривиальный характер: инкрементирует счётчик ошибок.
-double error(const std::string& error_message) {
+Number error(const std::string &error_message) {
   ++no_of_errors;
   std::cerr << "error: " << error_message << std::endl;
   return 1;
+  //TODO throw err_not_implement;
 }
 
-Token_value get_token(std::istream* input) {
+Token_value get_token(std::istream *input) {
   char ch;
 
   do {    // Пропустить все пробельные символы кроме '\n'.
@@ -47,22 +51,26 @@ Token_value get_token(std::istream* input) {
     case 0: // При вводе символа конца файла, завершить работу калькулятора.
       return curr_tok = END;
     case PRINT:
-    case '\n':
-      return curr_tok = PRINT;
-    case MUL:
-    case DIV:
-    case PLUS:
-    case MINUS:
-    case LP:
-    case RP:
-    case ASSIGN:
-      return curr_tok = Token_value(ch); // Приведение к целому и возврат.
-    case NUM0: case NUM1: case NUM2: case NUM3: case NUM4:
-    case NUM5: case NUM6: case NUM7: case NUM8: case NUM9:
-    case NUMP:
-      input->putback(ch); // Положить назад в поток...
-      *input >> number_value; // И считать всю лексему.
-      return curr_tok = NUMBER;
+    case '\n':return curr_tok = PRINT;
+      {//operation
+        case PLUS:
+        case FUNC:
+        case ASSIGN:return curr_tok = Token_value(ch); // Приведение к целому и возврат.
+      }
+      {//number
+        case NUM0:
+        case NUM1:
+        case NUM2:
+        case NUM3:
+        case NUM4:
+        case NUM5:
+        case NUM6:
+        case NUM7:
+        case NUM8:
+        case NUM9:input->putback(ch); // Положить назад в поток...
+        *input >> number_value; // И считать всю лексему.
+        return curr_tok = NUMBER;
+      }
     default:
       if (isalpha(ch)) {
         string_value = ch;
@@ -82,95 +90,86 @@ Token_value get_token(std::istream* input) {
  * очередной лексемы. */
 
 // prim() - обрабатывает первичные выражения.
-double prim(std::istream* input, bool get) {
+Number prim(std::istream *input, bool get) {
   if (get) {
     get_token(input);
   }
 
   switch (curr_tok) {
     case NUMBER: {
-      double v = number_value;
+      Number v = number_value;
       get_token(input);
       return v;
     }
     case NAME: {
-      double& v = table[string_value];
+      Number &v = table[string_value];
       if (get_token(input) == ASSIGN) {
-          v = expr(input, true);
+        v = expr(input, true);
       }
       return v;
     }
-    case MINUS:
-      return -prim(input, true);
+
     case LP: {
-      double e = expr(input, true);
+      Number e = expr(input, true);
       if (curr_tok != RP) {
-          return error("')' expected");
+        return error("')' expected");
       }
       get_token(input);
       return e;
     }
-    default:
-      return error("primary expected");
+    case FUNC://TODO throw not-implentetd
+    default:return error("primary expected");
   }
 }
 
 // term() - умножение и деление.
-double term(std::istream* input, bool get) {
-  double left = prim(input, get);
+Number term(std::istream *input, bool get) {
+  Number left = prim(input, get);
 
-  for ( ; ; ) {
+  for (;;) {
     switch (curr_tok) {
-      case MUL:
-        left *= prim(input, true);
+      case MUL:left *= prim(input, true);
         break;
       case DIV:
         if (double d = prim(input, true)) {
-            left /= d;
-            break;
+          left /= d;
+          break;
         }
         return error("Divide by 0");
-      default:
-          return left;
+      default:return left;
     }
   }
 }
 
 // expr() - сложение и вычитание.
-double expr(std::istream* input, bool get) {
-  double left = term(input, get);
+Number expr(std::istream *input, bool get) {
+  Number left = term(input, get);
 
-  for ( ; ; ) {
+  for (;;) {
     switch (curr_tok) {
       case PLUS:
+        print(PLUS);
         left += term(input, true);
         break;
-      case MINUS:
-        left -= term(input, true);
-        break;
-      default:
-        return left;
+      default:return left;
     }
   }
 }
+void print(Token_value value) {
+  std::cout << value;
+}
 
-int main(int argc, char* argv[]) {
-  std::istream* input = nullptr; // Указатель на поток.
+int main(int argc, char *argv[]) {
+  std::istream *input = nullptr; // Указатель на поток.
 
   switch (argc) {
-    case 1:
-      input = &std::cin;
+    case 1:input = &std::cin;
       break;
-    case 2:
-      input = new std::istringstream(argv[1]);
+    case 2:input = new std::istringstream(argv[1]);
       break;
-    default:
-      error("Too many arguments");
+    default:error("Too many arguments");
       return 1;
   }
-
-  table["pi"] = 3.1415926535897932385;
-  table["e"] = 2.7182818284590452354;
 
   while (*input) {
     get_token(input);
@@ -182,14 +181,13 @@ int main(int argc, char* argv[]) {
     if (curr_tok == PRINT) {
       continue;
     }
-
-    // expr() -> term() -> prim() -> expr() ...
+    //expr()->func()->prim()->expr() ....
     std::cout << expr(input, false) << std::endl;
   }
 
-  if (input != &std::cin) {
+  if (input != &std::cin) {//free memory if we need to.
     delete input;
   }
 
-  return no_of_errors;
+  return no_of_errors;//TODO print errors
 }
